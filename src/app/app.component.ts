110e87d5f3d684b6ca5617d28e9a00aa91dd59bb
Hari <ofc23hari@gmail.com>
2019-07-15 20:17:37 +0530

    angular component-interactions file add in seperate git

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+import { InteractionService } from "./interaction.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "angular-component-interactions";
+
+  constructor(private _interactionService: InteractionService) {}
+
+  greetStudent() {
+    this._interactionService.sendMessage("Good Morning");
+  }
+
+  appreciateStudent() {
+    this._interactionService.sendMessage("well Done");
+  }
+}
